7ee25bcfb9f110f93db062ecfc33887081a2c6aa
kellworks <kchidothi@outlook.com>
2023-09-11 09:00:04 +0200

    rename service

--- setup.sh
+++ setup.sh
@@ -26,55 +26,58 @@ fi
 install_dir="/usr/local/bin"
 
 script_name="main"
+service_name="HILabelPrinter"
 
-username=$(whoami)
 
-current_dir=$(pwd)
-
-if [ -f "$install_dir/$script_name" ]; then
-    echo "$script_name is already installed."
+if [ -f "$install_dir/$service_name" ]; then
+    echo "$service_name is already installed."
 else
     cp "./dist/$script_name" "$install_dir/"
 
-    chmod +x "$install_dir/$script_name"
+    mv "$install_dir/$script_name" "$install_dir/$service_name"
 
-    echo "$script_name has been installed in $install_dir."
+    # chmod +x "$install_dir/$script_name"
+    chmod +x "$install_dir/$service_name"
 
-    if systemctl is-active --quiet "$script_name.service"; then
-        echo "Stopping and disabling $script_name.service..."
-        systemctl stop "$script_name.service"
-        systemctl disable "$script_name.service"
+    echo "$service_name has been installed in $install_dir."
 
-        service_file="/etc/systemd/system/$script_name.service"
+    if systemctl is-active --quiet "$service_name.service"; then
+        echo "Stopping and disabling $service_name.service..."
+        systemctl stop "$service_name.service"
+        systemctl disable "$service_name.service"
+
+        service_file="/etc/systemd/system/$service_name.service"
         rm -f "$service_file"
 
-        echo "$script_name.service has been removed."
+        echo "$service_name.service has been removed."
     else
-        echo "$script_name.service does not exist."
+        echo "$service_name.service does not exist."
     fi
 
     if [[ "$(uname -s)" == "Linux" ]]; then
-        service_file="/etc/systemd/system/$script_name.service"
+        service_file="/etc/systemd/system/$service_name.service"
+        username=$(logname)
+
+        current_dir=$(pwd)
 
         echo "[Unit]" > "$service_file"
-        echo "Description=$script_name" >> "$service_file"
-        echo "After=network.target" >> "$`service_file`"
-        echo "Restart=always"
-        echo "User=$username"
+        echo "Description=$service_name" >> "$service_file"
+        echo "After=network.target" >> "$service_file"
         echo "" >> "$service_file"
         echo "[Service]" >> "$service_file"
-        echo "ExecStart=$install_dir/$script_name" >> "$service_file"
-        echo "WorkingDirectory=$current_dir"
+        echo "ExecStart=$install_dir/$service_name" >> "$service_file"
+        echo "WorkingDirectory=$current_dir"  >> "$service_file"
         echo "Restart=always" >> "$service_file"
+        echo "User=$username"  >> "$service_file"
         echo "" >> "$service_file"
         echo "[Install]" >> "$service_file"
         echo "WantedBy=multi-user.target" >> "$service_file"
 
         systemctl daemon-reload
-        systemctl enable "$script_name.service"
-        systemctl start "$script_name.service"
+        systemctl enable "$service_name.service"
+        systemctl start "$service_name.service"
 
-        echo "$script_name has been configured to run on startup (systemd)."
+        echo "$service_name has been configured to run on startup (systemd)."
     fi
 fi
 echo "✨️ Setup successfully completed..."
